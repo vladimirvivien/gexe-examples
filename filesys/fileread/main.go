032022da96b95ed46d2d03b36b85ee8b57a5160a
Vladimir Vivien <vladimir.vivien@gmail.com>
2024-12-20 10:09:44 -0500

    Fix link and improve output

--- filesys/fileread/main.go
+++ filesys/fileread/main.go
@@ -7,20 +7,20 @@ import (
 	"github.com/vladimirvivien/gexe"
 )
 
-// This example shows how to use the fs package to create/write
-// files with simplicity.
+// This example shows how to use file operations with gexe.
 func main() {
-	fmt.Println("Downloading and saving W. E. Du Bois text to /tmp/dubois-souls.txt")
+	gexe.SetVar("file", "/tmp/warofworlds.txt")
+	fmt.Prinln(gexe.Eval("Downloading and saving War of the Worlds text: $file"))
 
-	// Download W. E. Du Bois text from Gutenberg and save locally
-	cmd := `wget -O /tmp/dubois-souls.txt  https://www.gutenberg.org/cache/epub/408/pg408.txt`
+	// Download text and save it locally
+	cmd := `wget -O $file  https://www.gutenberg.org/cache/epub/36/pg36.txt`
 	if err := gexe.RunProc(cmd).Err(); err != nil{
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// read the text display the text
-	if err := gexe.FileRead("/tmp/dubois-souls.txt").Into(os.Stdout).Err(); err != nil {
+	// read the dowloaded file and stream it to stdout
+	if err := gexe.FileRead("$file").Into(os.Stdout).Err(); err != nil {
 		fmt.Println(err)
 	}
 }
